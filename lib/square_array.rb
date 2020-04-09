77075df8982b6f9eeab9b7aab2bace91ca28676f
JC Ramirez <jsmnramirez25@gmail.com>
2020-04-09 00:47:44 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,24 @@
+require "pry"
+
 def square_array(array)
-  # your code here
+  i = 0 
+  new_array = []
+  while i < array.length do
+    bannana = array[i] * array[i]
+   new_array << bannana 
+   i += 1 
+   
+  end
+  new_array
+  
 end
+
+
+
+#puts square_array [1,2,3]
+
+
+
+
+
+
